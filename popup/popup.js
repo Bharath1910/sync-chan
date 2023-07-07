67077d35f10a31e9c354d93c8ded5a9ed746e5fc
Bharath1910 <av.bharath@proton.me>
2023-07-07 15:19:31 +0530

    popup testing

---
+++ popup/popup.js
@@ -0,0 +1,9 @@
+animeName = document.getElementById("anime-name");
+
+
+chrome.runtime.onMessage.addListener(
+    function (req, sender, sendRes) {
+        console.log(req.animeName)
+        animeName.innerHTML = req.animeName;
+    }
+)
