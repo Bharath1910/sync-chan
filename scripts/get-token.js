76ce9d345b7e922ecdfbd4525122c62e2bf5bcfd
Bharath1910 <av.bharath@proton.me>
2023-07-07 18:28:05 +0530

    added tokens to storage

--- scripts/get-token.js
+++ scripts/get-token.js
@@ -4,10 +4,10 @@ let refreshToken = document.getElementById('refresh-token').innerText;
 console.log("testing! get-token here!");
 console.log(accessToken, refreshToken);
 
-(async () => {
-    const res = await chrome.runtime.sendMessage({
-        type: 'get-token',
-        accessToken,
-        refreshToken
-    })
-})();
+chrome.storage.onChanged.addListener()
+chrome.storage.local.set({
+    'accessToken': accessToken,
+    'refreshToken': refreshToken
+}, () => {
+    console.log("tokens saved")
+})
