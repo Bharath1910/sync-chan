976a5f31e4894e279aa8a6940b4fa79877a03dd7
Bharath1910 <av.bharath@proton.me>
2023-07-08 13:16:49 +0530

    made the popup look good

--- background.js
+++ background.js
@@ -4,5 +4,33 @@ chrome.runtime.onMessage.addListener(
             console.log("testing! in the if statement!")
             console.log(req.accessToken, req.refreshToken)
         }
+
+        if (req.type === 'title') {
+            console.log("ON TITLE")
+    
+            console.log(req.animeName)
+            console.log(req.episode)
+        }
+        
+        else {
+            console.log(req.form)
+        }
     }
 )
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url == undefined) {
+        console.log("undefined")
+        return
+    }
+
+    const animeDetails = changeInfo.url.split('/')[4];
+    const animeName = animeDetails.split('?')[0].split('-').slice(0, -1).join(' ');
+
+    (async () => {
+    await chrome.runtime.sendMessage({
+        type: 'title',
+        animeName: animeName
+    })
+    })();
+})
--- popup/scripts/page.index.js
+++ popup/scripts/page.index.js
@@ -1,5 +1,27 @@
 import { renderHTML } from "./utils.js";
 
 const navbar = document.getElementsByTagName('nav')[0];
+const footer = document.getElementsByTagName('footer')[0];
 
 renderHTML('../documents/template.header.html', navbar);
+renderHTML('../documents/template.footer.html', footer);
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === 'title') {
+        console.log("ON TITLE")
+
+        console.log(request.animeName)
+        const image = document.getElementById('not-found');
+        image.style.display = 'hidden';
+        image.style.visibility = 'hidden';
+
+        const text = document.getElementById('mag-icon');
+        text.style.display = 'hidden';
+        text.style.visibility = 'hidden';
+
+        let title = document.getElementById('title');
+        title.innerHTML = request.animeName;
+    }
+})
